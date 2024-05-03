e2948eead9d7f1c2c3782f3d8e4c313a121af13f
Richard Kužela <richard@kanshen.click>
2024-05-03 15:11:28 +0200

    Added some basic section changing script

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('#menu-oFarnosti').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "oFarnosti") });
+document.querySelector('#menu-svätéOmše').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "svätéOmše") });
+document.querySelector('#menu-farskéOznamy').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "farskéOznamy") });
+document.querySelector('#menu-aktuality').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "aktuality") });
+document.querySelector('#menu-fotogaléria').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "fotogaléria") });
+document.querySelector('#menu-kontakty').addEventListener('click', () => { document.querySelector("div#content").setAttribute("current", "kontakty") });
