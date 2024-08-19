1a8b7ea6515686e0d7c0e1066fc8ccc2da3ac9ff
出口レイ <richard@kanshen.click>
2024-08-19 10:31:38 +0200

    Fixed a grammar mistake

--- src/pages/OFarnosti/oFarnosti.js
+++ src/pages/OFarnosti/oFarnosti.js
@@ -11,7 +11,7 @@ export default function OFarnosti() {
             <div id='contentWrapper'>
                 <SvateOmse />
                 <p>
-                    <strong>Farský kostol Najsvätejšej Trojice</strong> v obci Sverepec je jej hlavnou dominantou. Jeho základný kameň požehnal papež Ján Pavol II. 20. 4. 1990, počas svojej návštevy v Bratislave. Kostol bol postavený s iniciatívy dekana Pavla Trnku, ktorý takto naplnil túžbu obyvateľov obce mať svoj vlastný kostol. Vďaka patrí aj všetkým ostatným rodákom, živým a dnes už aj mŕtvym, ktorí pomáhali pri jeho výstavbe prácou a finančne.<strong> Kostol Najsvätejšej Trojice vysvätil dňa 26. mája 2002</strong> Ján Chryzostom kardinál Korec, nitriansky biskup.
+                    <strong>Farský kostol Najsvätejšej Trojice</strong> v obci Sverepec je jej hlavnou dominantou. Jeho základný kameň požehnal papež Ján Pavol II. 20. 4. 1990, počas svojej návštevy v Bratislave. Kostol bol postavený z iniciatívy dekana Pavla Trnku, ktorý takto naplnil túžbu obyvateľov obce mať svoj vlastný kostol. Vďaka patrí aj všetkým ostatným rodákom, živým a dnes už aj mŕtvym, ktorí pomáhali pri jeho výstavbe prácou a finančne.<strong> Kostol Najsvätejšej Trojice vysvätil dňa 26. mája 2002</strong> Ján Chryzostom kardinál Korec, nitriansky biskup.
                     <br /><br />
                     Kostol je <strong>jednoloďovou stavbou so samostatnou vežou</strong>, priestor so schodiskom medzi kostolom a vežou je zastrešený. V otvorenej veži sú umiestnené <strong>tri zvony</strong>. Vpravo pred kostolom je vytvorená<strong> jaskyňa so sochou Panny Márie</strong>, na terase nad jaskyňou je <strong>bronzové súsošie so svätým Jozefom</strong>.
                     <br /><br />
